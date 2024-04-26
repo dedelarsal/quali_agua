358413b6f817e335c27d0229e8d816e41a09971e
dedelarsal <adelson.arsal@gmail.com>
2024-04-26 08:43:26 -0300

    identifica no popup o parâmetro fora do padrão

--- mapa/utils.py
+++ mapa/utils.py
@@ -5,23 +5,40 @@ def make_markers_and_add_to_map(map, df):
     flag = folium.Icon(icon='fa-vial-circle-check', prefix='fa', color='green')
     for i in df.parametro.unique():
         for id, row in df[df['parametro'] == i].iterrows():
-            popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
-
             if row.parametro == 'Coliformes Totais':
                 if float(row.valor_parametro) > 0:
+                    popup += "<p style='color:red;''>" + row.parametro + " = " + row.valor_parametro + "</p>"
                     flag = folium.Icon(icon='vial-virus', prefix='fa', color='red')
+                else:
+                    popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
+
             if row.parametro == 'E. coli':
                 if float(row.valor_parametro) > 0:
+                    popup += "<p style='color:red;''>" + row.parametro + " = " + row.valor_parametro + "</p>"
                     flag = folium.Icon(icon='vial-virus', prefix='fa', color='red')
+                else:
+                    popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
+
             if row.parametro == 'Cloro Residual Livre (mg/L)':
                 if float(row.valor_parametro) < 0.2 or float(row.valor_parametro) > 5:
+                    popup += "<p style='color:red;''>" + row.parametro + " = " + row.valor_parametro + "</p>"
                     flag = folium.Icon(icon='vial-virus', prefix='fa', color='red')
+                else:
+                    popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
+
             if row.parametro == 'Turbidez (uT)':
                 if float(row.valor_parametro) > 5:
+                    popup += "<p style='color:red;''>" + row.parametro + " = " + row.valor_parametro + "</p>"
                     flag = folium.Icon(icon='vial-virus', prefix='fa', color='red')
+                else:
+                    popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
+
             if row.parametro == 'Cor Aparente (uH)':
                 if float(row.valor_parametro) > 15:
+                    popup += "<p style='color:red;''>" + row.parametro + " = " + row.valor_parametro + "</p>"
                     flag = folium.Icon(icon='vial-virus', prefix='fa', color='red')
+                else:
+                    popup += "<p>" + row.parametro + " = " + row.valor_parametro + "</p>"
 
     folium.Marker(
             location = [df.latitude.unique()[0], df.longitude.unique()[0]],
